61b3422db12f22958a0af60357d59772c94c9d2f
democr1t <demon.krut98@gmail.com>
2023-10-01 14:33:46 +0300

    solved spinned

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
 
-var button = document.getElementById("submit")
-button.addEventListener('submit', (evt) => {
+const button = document.getElementById('submit')
+console.log(button)
+let isLoading = false;
+
+button.addEventListener('click', () => {
+    const root = document.querySelector(":root");
+
+    isLoading ? isLoading = false : isLoading = true;
+
+    if(isLoading)
+    {
+        root.style.setProperty("--pseudo-display", 'inline-block');
+        button.innerHTML = "";
+    }
+    else
+    {
+        root.style.setProperty("--pseudo-display", 'none');
+        button.innerHTML = "Submit";
+    }
+
 })
