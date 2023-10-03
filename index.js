139ebd3b6b8600bf5450c1e63599adfcfb73f0eb
democr1t <32939293+democr1t@users.noreply.github.com>
2023-10-03 17:31:21 +0300

    solved 4/6

--- index.js
+++ index.js
@@ -18,3 +18,21 @@ button.addEventListener('click', () => {
         button.innerHTML = "Submit";
     }
 })
+
+
+const typeButton = document.getElementById('typewrite')
+typeButton.addEventListener('click', () =>{
+    const spanElement = document.getElementById('textToWrite')
+    const spanText = spanElement.innerText;
+
+    let i = 0;
+
+    spanElement.innerText = "";
+    
+    const interval = setInterval(() => {
+        spanElement.innerText += spanText[i]
+        i == spanText.length - 1 ? clearInterval(interval) : i++
+        console.log(i)
+        //если успеть нажать еще раз, то текст берется текущий, хотя переменная константа, почему?
+    }, 10);
+})
